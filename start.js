e6425ca4f2dd52346b05ac7e9a6e973c5b171ed7
Melih Gurgah <melih.gurgah@aurea.com>
2020-02-16 22:47:58 +0300

    port set to 80

--- start.js
+++ start.js
@@ -1,3 +1,3 @@
 
 const app = require('./app.js')
-app.listen(8080, () => console.log(`Started server at http://localhost:8080!`));
+app.listen(80, () => console.log(`Started server at http://localhost:80!`));
